c2e89b69ae545b93ee8c048dd4ce0605ed61c6b8
Dafa Dwi Antara <116301349+ntrdf@users.noreply.github.com>
2022-10-24 14:27:06 +0700

    Add files via upload

---
+++ Tugas.java
@@ -0,0 +1,14 @@
+public class Tugas{
+    public static void main(String[] args) {
+        for(Integer i =1;i <= 5;i++){
+            if(i % 2 == 1){
+                System.out.println("Angka " + i + " adalah Ganjil");
+            }
+            else
+            {
+                System.out.println("Angka " + i + " adalah Genap");
+            }
+            
+        }
+    }
+}
